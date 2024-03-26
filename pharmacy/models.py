c644a4b914a0d1ea7998471555a8b7050de42084
Nissi Paul Mikkili <nissipaulmikkili@Nissis-M2-MacBook-Air.local>
2024-03-26 11:17:51 +0530

    added a table stockopenings

--- pharmacy/models.py
+++ pharmacy/models.py
@@ -1,3 +1,14 @@
+from datetime import timezone
 from django.db import models
 
 # Create your models here.
+class StockOpenings(models.Model):
+    description = models.CharField(max_length=100, blank=False)
+    units_per_strip= models.IntegerField(blank=False)
+    packing= models.CharField(max_length=100, blank=False)
+    batch= models.CharField(max_length=100, blank=False)
+    mrp_per_strip= models.FloatField(blank=False)
+    expiry_date= models.DateField(blank=False)
+    imported_strips= models.IntegerField(blank=False)
+    date_of_stock_openings= models.DateField(default=timezone.now)
+
